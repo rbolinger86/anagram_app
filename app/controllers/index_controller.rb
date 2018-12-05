fe0e351dab9be0452b2c4cd1e407af976481f8a6
Robert Bolinger <rbolinger86@gmail.com>
2018-12-05 16:45:38 -0600

    No uniq method?

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -9,7 +9,36 @@ get '/anagrams/:word' do
   erb :show
 end
 
+def three_letters?(input)
+  if input.length < 4
+    true
+  else
+    false
+  end
+end
+def distinct_letters?(input)
+  letter_array = input.chars
+  unique_letters = letter_array.uniq
+  if unique_letters.length < letter_array.length
+    false
+  else
+    true
+  end
+end
+def valid_input?(input)
+  if three_letters?(input) && distinct_letters?(input)
+    true
+  else
+    false
+  end
+end
+
 post '/' do
-  @word = params[:word]
-  redirect "/anagrams/#{@word}"
+  word = params[:word]
+  if valid_input?(word)
+    redirect "/anagrams/#{word}"
+  else
+    @error = "Sorry, your input was invalid. Please try again!"
+    erb :index
+  end
 end

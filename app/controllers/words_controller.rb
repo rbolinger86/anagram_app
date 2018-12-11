2135e88c168cbcbf07a3aae3ba676e23b040e2f6
Robert Bolinger <rbolinger86@gmail.com>
2018-12-10 18:03:43 -0600

    Words not being created!

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,6 +3,15 @@ get '/words' do
   erb :"/words/index"
 end
 
+get '/words/new' do
+  erb :"/words/new"
+end
+
+post '/words' do
+  word = Word.create(text: params[:text])
+  redirect "/words/#{word.id}"
+end
+
 get '/words/:id' do
   @word = Word.find(params[:id])
   erb :"/words/show"
